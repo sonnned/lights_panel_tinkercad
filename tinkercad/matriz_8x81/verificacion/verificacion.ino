f0f476cfda2962ea4a55babc053fe71dade80d45
sergio-alarcon1 <sergio.alarcon1@udea.edu.co>
2023-09-23 08:40:25 -0500

    modificar funcion verificacion, y implementacion de la funcion mostrador
    mostrador efectua el patron de una funcion, contando la veces que se hace, y el intervalo de tiempo que se prende y se apaga la matriz

--- tinkercad/matriz_8x81/verificacion/verificacion.ino
+++ tinkercad/matriz_8x81/verificacion/verificacion.ino
@@ -1,51 +1,16 @@
 
 
 void verificacion() {
- 
-    
-  bool condicion=true;
-  unsigned long tiempo;
-  int vuelta=0;
-  unsigned long repe;
-  unsigned long intervalo;
-  
-  Serial.begin(9600);
-  
-  Serial.println("Ingrese el numero de repeticiones que quiere(entero positivo): ");
-  while (!Serial.available()) {}
-  repe =Serial.parseInt();
-  
-  Serial.println("Ingrese el intervalo (en segundos): ");
-  while (!Serial.available()) {}
-  intervalo = 1000 * Serial.parseInt();
-  
 
- tiempo=millis();
-  while(vuelta<repe){
-    if(millis()-tiempo<intervalo){
-      if(condicion){
-      digitalWrite(reloj_salida, LOW);
-      shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0b11111111);
-  	  shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0);
-	  digitalWrite(reloj_salida, HIGH);
-      }
-    }
-    else{
-      digitalWrite(reloj_salida, LOW);
-      shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0);
-  	  shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0);
-	  digitalWrite(reloj_salida, HIGH);
-       condicion=!condicion;
-       tiempo=millis();
-        if(condicion){
-        vuelta++;
-        }
-      }
-    
-  
-  }
-    
-  }
-  
+
+  digitalWrite(reloj_salida, LOW);
+  shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0b11111111);
+  shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0);
+  digitalWrite(reloj_salida, HIGH);
+
+}
+
+}    
+ 
 
 
