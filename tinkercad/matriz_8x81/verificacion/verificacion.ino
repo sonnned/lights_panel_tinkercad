b97ab43cc49b46d73fb9406f026c3d2696573d01
sergio-alarcon1 <sergio.alarcon1@udea.edu.co>
2023-09-23 15:21:48 -0500

    modificación de función verificacion

--- tinkercad/matriz_8x81/verificacion/verificacion.ino
+++ tinkercad/matriz_8x81/verificacion/verificacion.ino
@@ -1,16 +1,44 @@
 
 
+
 void verificacion() {
-
-
-  digitalWrite(reloj_salida, LOW);
-  shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0b11111111);
-  shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0);
-  digitalWrite(reloj_salida, HIGH);
-
-}
-
-}    
+  bool condicion=true;
+  unsigned long tiempo;
+  int vuelta=0;
+  unsigned long repe=2;
+  unsigned long intervalo=1000;
+ tiempo=millis();
+  while(vuelta<repe){
+    if(millis()-tiempo<intervalo){
+      if(condicion){
+       digitalWrite(reloj_salida, LOW);
+      shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0b11111111);
+  	  shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0);
+	  digitalWrite(reloj_salida, HIGH);
+  		
+      }
+    }
+    else{
+      digitalWrite(reloj_salida, LOW);
+      shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0);
+  	  shiftOut(entrada, reloj_desplazamiento, MSBFIRST, 0);
+	  digitalWrite(reloj_salida, HIGH);
+       condicion=!condicion;
+       tiempo=millis();
+        if(condicion){
+        vuelta++;
+        }
+      }
+    
+  
+  }
+ 
+    
+    
+    
+  }
+  
+  
  
 
 
