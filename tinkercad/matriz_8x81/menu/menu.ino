63cf3f45ad75d106ef4aba5b3d7082ec9788cd24
sonnned <alejo1garciasosa@gmail.com>
2023-09-23 20:23:38 -0500

    Menu

--- tinkercad/matriz_8x81/menu/menu.ino
+++ tinkercad/matriz_8x81/menu/menu.ino
@@ -1,40 +1,34 @@
+void publik() {
+    Serial.println("Opciones del menu:");
+    Serial.println("1. verificar matriz");
+    Serial.println("2. digitar imagen");
+    Serial.println("3. verificar matriz por usuario");
+    Serial.println("4. elegir imagen ");
+    Serial.println("5. mostrar patrones ");
+    Serial.print("Seleccione una opcion: ");
 
-void mostrarMenu() {
- 
-  Serial.println("Opciones del menu:");
-  Serial.println("1. verificar matriz");
-  Serial.println("2. digitar imagen");
-  Serial.println("3. verificar matriz por usuario");
-  Serial.println("4. elegir imagen ");
-  Serial.println("5. mostrar patrones ");
- 
+    while (!Serial.available()) {}
+    int opcion = Serial.parseInt();
+    Serial.println(opcion);
 
-
-  Serial.print("Seleccione una opcion: ");
-  while (!Serial.available()) {
-   
-  }
-
-  int opcion = Serial.parseInt(); 
-  
-  switch (opcion) {
-    case 1:
-  verificacion();
-  
-      break;
-    case 2:
-    
-      break;
-    case 3:
-      break;
-    case 4:
-    
-      break;
-    case 5:
-    
-      break;
-    default:
-    
-      Serial.println("Opción no válida");
-  }
+    switch (opcion) {
+        case 1:
+            verificacion();
+            break;
+        case 2:
+            user_patterns();
+            break;
+        case 3:
+            mostrador(verificacion);
+            break;
+        case 4:
+            delay_millis();
+            break;
+        case 5:
+            delay_millis();
+            break;
+        default:
+            Serial.println("Opción no válida");
+            break;
+    }
 }
